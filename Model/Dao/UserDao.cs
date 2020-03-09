00ea4cce4dbbe94faec65df9817490c55e449fe0
= <=>
2020-03-09 22:26:37 +0700

    Add login for Admin

---
+++ Model/Dao/UserDao.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    public class UserDao
+    {
+        OnlineShopDBContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDBContext();
+        }
+        public long Insert(User entity)
+        {
+            db.User.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+        public User GetByID(string userName)
+        {
+            return db.User.SingleOrDefault(x=>x.UserName == userName);
+        }
+        public int Login(String userName, string passWord)
+        {
+            var result = db.User.SingleOrDefault(x => x.UserName == userName);
+            if (result == null)
+            {
+                return 0; //Account doesn't exist
+            }
+            else
+            {
+                if(result.Status == false)
+                {
+                    return -1; //Account locked
+                }
+                else
+                {
+                    if(result.Password == passWord)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return -2; //Incorrect password
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using OnlineShop.Areas.Admin.Models;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public string CommonConstant { get; private set; }
+
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new Model.Dao.UserDao();
+                var result = dao.Login(model.UserName, Encryptor.MD5Hash(model.Password));
+                if (result == 1)
+                {
+                    var user = dao.GetByID(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserID = user.ID;
+
+                    Session.Add(Common.CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else if(result == 0)
+                {
+                    ModelState.AddModelError("", "Account doesn't exist!");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Account locked!");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Incorrect password!");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Error Login!");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
