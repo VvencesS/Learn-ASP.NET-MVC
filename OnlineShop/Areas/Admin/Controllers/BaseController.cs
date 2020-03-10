a96a36dde7ee9da261ec7c3259473862320fe904
= <=>
2020-03-10 10:08:02 +0700

    Add BaseController, check session in Admin, if user enters url of any page, the system will navigate to Login page

---
+++ OnlineShop/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        public object SessionHelper { get; private set; }
+        public object RouteValueDicrectionary { get; private set; }
+
+        /*
+         * Check session in Admin, if user enters url of any page, the system will navigate to Login page
+         */
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+            if(session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new 
+                    RouteValueDictionary(new { Controller = "Login", action = "Index", Area = "Admin" }));
+            }
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
